f8b7e699f0c2d073a8ee1efe69b9b0eef7e28df3
Rahul250192 <gupta.rahulg25@gmail.com>
2019-02-11 00:55:44 -0700

    adding weights

--- variationalAutoEncoder.py
+++ variationalAutoEncoder.py
@@ -16,3 +16,22 @@ image_dim = 784 # MNIST images are 28x28 pixels
 hidden_dim = 512
 latent_dim = 2
 
+#using Xavier Glorot initialization
+def glorot_init(shape):
+    return tf.random_normal(shape=shape, stddev=1. / tf.sqrt(shape[0] / 2.))
+
+# Weights and Biases
+weights = {
+    'encoder_h1': tf.Variable(glorot_init([image_dim, hidden_dim])),
+    'z_mean': tf.Variable(glorot_init([hidden_dim, latent_dim])),
+    'z_std': tf.Variable(glorot_init([hidden_dim, latent_dim])),
+    'decoder_h1': tf.Variable(glorot_init([latent_dim, hidden_dim])),
+    'decoder_out': tf.Variable(glorot_init([hidden_dim, image_dim]))
+}
+biases = {
+    'encoder_b1': tf.Variable(glorot_init([hidden_dim])),
+    'z_mean': tf.Variable(glorot_init([latent_dim])),
+    'z_std': tf.Variable(glorot_init([latent_dim])),
+    'decoder_b1': tf.Variable(glorot_init([hidden_dim])),
+    'decoder_out': tf.Variable(glorot_init([image_dim]))
+}
